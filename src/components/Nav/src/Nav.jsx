92044363cf37ab017a258aac63aeb135d49206c9
Martin Marzejon <martinmarzejon@Martins-MacBook-Pro.local>
2023-01-23 22:44:03 -0800

    fix: nav

--- src/components/Nav/src/Nav.jsx
+++ src/components/Nav/src/Nav.jsx
@@ -3,6 +3,10 @@ import { NavItem } from '../index';
 export default function Nav(props) {
   const { menu } = props;
 
+  if (!menu) {
+    return null;
+  }
+
   return (
     <nav className="hidden lg:min-h-screen lg:inset-y-0 lg:px-4 lg:pt-5 lg:pb-4 lg:flex lg:w-64 lg:flex-col lg:border-r lg:border-gray-200 lg:bg-gray-100">
       <div className="space-y-1">
